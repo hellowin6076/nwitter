a4e0da8a94e6398321e4e526896a91bb885693d7
lee.huimok <hellowin6076@gmail.com>
2021-01-17 12:10:35 +0900

    1.3.3 Realtime Nweets

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React,{ useEffect, useState } from  "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(()=>{
+  const [userObj, setUserObj] = useState(null);
+  useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      if(user){
+      if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -16,10 +18,10 @@ function App() {
     });
   }, []);
   return (
-  <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn} />  : "Initialiging..."}
-    <footer>&copy; {new Date().getFullYear()} Bufgix</footer>
-  </>
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initialiging..."}
+      <footer>&copy; {new Date().getFullYear()} Bufgix</footer>
+    </>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
-    const [nweet, setNweet] = useState();
-    const onSubmit = (event) => {
+const Home = ({ userObj }) => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+        dbService.collection("nweets").onSnapshot(snapShot => {
+            const nweetArray = snapShot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setNweets(nweetArray);
+        });
+    }, []);
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("nweets").add({
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+        });
+        setNweet("");
     };
     const onChange = (event) => {
         const { target: { value } } = event;
@@ -12,9 +37,22 @@ const Home = () => {
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input value={nweet} onChange={onChange} type="text" placeholder="What's on you mind?" maxLength={120} />
+                <input
+                    value={nweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on you mind?"
+                    maxLength={120}
+                />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.text}</h4>
+                    </div>
+                    ))}
+            </div>
         </div>
     );
 };
