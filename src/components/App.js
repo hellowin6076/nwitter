eb68bc88377061fef97011715680a48a524a9de0
lee.huimok <hellowin6076@gmail.com>
2021-01-17 15:39:01 +0900

    1.3.5 Delete and Update

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,18 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initialiging..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initialiging..."}
       <footer>&copy; {new Date().getFullYear()} Bufgix</footer>
     </>
   );
